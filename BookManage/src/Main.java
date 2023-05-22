a9d5fd4e3ae277881bd93d2067f1e944d04965d5
kim980227 <sear227@gmail.com>
2023-05-22 14:44:49 +0900

    jdbc 연결

--- BookManage/src/Main.java
+++ BookManage/src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/db01";
+        String userName = "root";
+        String password = "1234";
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from t_book");
+
+        resultSet.next();
+        String book_id = resultSet.getString("book_name");
+        System.out.println(book_id);
+
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 }
