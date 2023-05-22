bc2daa5055721dcb59a45d13fee9faaf119541da
wkdtndgns <wkdtndgns@naver.com>
2023-05-22 17:41:11 +0900

    temp

--- BookManage/src/Book/BookRepository.java
+++ BookManage/src/Book/BookRepository.java
@@ -8,10 +8,10 @@ import java.util.LinkedList;
 
 public class BookRepository {
 
-    public LinkedList<Book> getAllByIdAndCategoryAndName() throws SQLException {
+    public LinkedList<Book> getAllByIdAndCategoryAndName(int Limit) throws SQLException {
         JdbcComm jdbc = new JdbcComm();
         Statement statement = jdbc.getConnection().createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT book_id, category, book_name FROM t_book");
+        ResultSet resultSet = statement.executeQuery("SELECT book_id, category, book_name FROM t_book LIMIT" + Limit );
         LinkedList<Book> liBook = new LinkedList<>();
 
         while (resultSet.next()) {
@@ -26,5 +26,18 @@ public class BookRepository {
         statement.close();
         jdbc.closeConnection();
         return liBook;
+    }
+
+    public int insert(Book book) throws SQLException {
+        JdbcComm jdbc = new JdbcComm();
+        Statement statement = jdbc.getConnection().createStatement();
+
+        String insertQuery = "INSERT INTO t_book (category, book_name, summary, author, publisher, in_date, is_in) " +
+                "VALUES (" + book.getCategory() + ", '" + book.getBookName() + "', '" + book.getSummary() + "', '" + book.getAuthor() + "', '" + book.getPublisher() + "', '" + book.getInDate() + "', '" + book.getIsIn() + "')";
+
+        int result = statement.executeUpdate(insertQuery);
+        statement.close();
+        jdbc.closeConnection();
+        return result;
     }
 }
